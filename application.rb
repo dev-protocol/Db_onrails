c5e7391649831139205fc5250a85d8405addc834
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-05-24 14:04:17 +0200

    Only load

--- application.rb
+++ application.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 require_relative 'boot'
 
+# Based on https://github.com/rails/rails/blob/v6.0.1/railties/lib/rails/all.rb
+# Only load the railties we need instead of loading everything
+require 'rails'
