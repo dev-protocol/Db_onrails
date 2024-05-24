4af20ac71ca47a6a5733414ef9cc766898b7719e
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-05-24 14:04:23 +0200

    active_record

--- application.rb
+++ application.rb
@@ -4,3 +4,5 @@ require_relative 'boot'
 # Based on https://github.com/rails/rails/blob/v6.0.1/railties/lib/rails/all.rb
 # Only load the railties we need instead of loading everything
 require 'rails'
+
+require 'active_record/railtie'
